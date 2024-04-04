5fb323bf9f45755a4c442d7ea41dcb0647fad25a
Jakob-Jorgensen <jakob.jorgensen01@gmail.com>
2024-04-04 12:06:50 +0200

    updating kinematic

--- implementation/kinematic.py
+++ implementation/kinematic.py
@@ -1,50 +1,47 @@
-import math 
+import numpy as np
 
-class Kinematic(object): 
+def Denavit_Hartenberg():
+    # Define Denavit-Hartenberg parameters 
+    Denvit_Hartenberg = {"theta": "rad", "r": "m", "d": "m", "alpha": "rad"}
+    Denvit_Hartenberg["theta"] = [0, 0, 0, 0, 0, 0]
+    Denvit_Hartenberg["r"] = [0, -0.6127, -0.57155, 0, 0, 0]
+    Denvit_Hartenberg["d"]= [0.1807, 0, 0, 0.17415, 0.11985, 0.11655]
+    Denvit_Hartenberg["alpha"] = [np.pi/2, 0, 0, np.pi/2, -np.pi/2, 0] 
+
+    return Denvit_Hartenberg
+
+
+#def transformation(i):
+def  transformation(theta,r,d,alpha): 
+
+    # Define transformation matrix  
+    """" 
+    theta=np.array(Denavit_Hartenberg()["theta"])
+    r=np.array(Denavit_Hartenberg()["r"])  
+    d=np.array(Denavit_Hartenberg()["d"]) 
+    alpha=np.array(Denavit_Hartenberg()["alpha"])   
     """ 
-    This class is used to calculate the Denavit-Hartenberg parameters and the Forward Kinematics for the UR10e robot.
-    """   
+    transformation = [[np.cos(theta), -np.sin(theta) * np.cos(alpha), np.sin(theta) * np.sin(alpha), r * np.cos(theta)], [np.sin(theta), np.cos(theta) * np.cos(alpha), -np.cos(theta) * np.sin(alpha), r * np.sin(alpha)], [0, np.sin(alpha), np.cos(alpha), d], [0, 0, 0, 1]]    
+    return transformation
 
-    def __init__(self):  
-        self.Denavit_Hartenberg() 
-         
-    def Denavit_Hartenberg(self):  
-        """  
-        This def  is used to calculate the Denavit-Hartenberg parameters for the UR10e robot.  
-        theta (list): The joint angles in radians. 
-        a (list): The link lengths. 
-        d (list): The link offsets. 
-        alpha (list): The link twist angles in radians.
-        """ 
-        self.joints = {"theta": "[rad]" , "r": "[m]", "d": "[m]", "alpha" : "[rad]"} # joints represents the Denavit-Hartenberg parameters and there units
-        self.joints["theta"] = [0,0,0,0,0,0] 
-        self.joints["r"] = [0, -0.6127, -0.57155, 0, 0, 0] 
-        self.joints["d"] = [0.1807, 0, 0, 0.17415, 0.11985, 0.11655] 
-        self.joints["alpha"] = [(math.pi)/2, 0, 0, math.pi/2, -math.pi/2, 0] 
-        return self.joints
-    def Forward_Kinematics(self):  
-        """  
-        This def is used to calculate the Forward Kinematics for the UR10e robot.  
-        theta (list): The joint angles in radians. 
-        a (list): The link lengths. 
-        d (list): The link offsets. 
-        alpha (list): The link twist angles in radians.
-        """    
-        homogenous_matrix = []  # Define the variable "homogenous_matrix"
-        for i in range(6):
-           
-        
-            #make me a 2d array of 4x4 matrix as the momogenous matrix 
-            homogenous_matrix *= [[math.cos(self.joints["theta"][i]), -math.sin(self.joints["theta"][i])*math.cos(self.joints["alpha"][i]), math.sin(self.joints["theta"][i])*math.sin(self.joints["alpha"][i]), self.joints["r"][i]*math.cos(self.joints["theta"][i])],
-                                 [math.sin(self.joints["theta"][i]), math.cos(self.joints["theta"][i])*math.cos(self.joints["alpha"][i]), -math.cos(self.joints["theta"][i])*math.sin(self.joints["alpha"][i]), self.joints["r"][i]*math.sin(self.joints["theta"][i])],
-                                 [0, math.sin(self.joints["alpha"][i]), math.cos(self.joints["alpha"][i]), self.joints["d"][i]],
-                                 [0, 0, 0, 1]]
-              
-              # homogenous_matrix = ([[math.cos(self.joints["theta"][i]),-math.sin(self.joints["theta"][i])*math.cos(self.joints["alpha"][i]),math.sin(self.joints["theta"][i])*math.sin(self.joints["alpha"][i]),self.joints["r"][i]*math.cos(self.joints["theta"][i])], 
-              #                  [math.sin(self.joints["theta"][i]),math.cos(self.joints["theta"][i])*math.cos(self.joints["alpha"][i]),-math.cos(self.joints["theta"][i])*math.sin(self.joints["alpha"][i]),self.joints["r"][i]*math.sin(self.joints["theta"][i])], 
-              #                  [0,math.sin(self.joints["alpha"][i]),math.cos(self.joints["alpha"][i]),self.joints["d"][i]],[0,0,0,1]])
-            
-        print(homogenous_matrix)
+def Forward_Kinematics(theta): 
+    # Calculate Forward Kinematics from base joint to the sixth joint 
 
-joints=Kinematic().Forward_Kinematics()
-#print(joints)
+    r=np.array(Denavit_Hartenberg()["r"])  
+    d=np.array(Denavit_Hartenberg()["d"]) 
+    alpha=np.array(Denavit_Hartenberg()["alpha"])  
+    Forward = np.matmul(np.matmul(np.matmul(np.matmul(np.matmul(transformation(theta[0], r[0], d[0], alpha[0]), transformation(theta[1], r[1], d[1], alpha[1])), transformation(theta[2], r[2], d[2], alpha[2])), transformation(theta[3], r[3], d[3], alpha[3])), transformation(theta[4], r[4], d[4], alpha[4])), transformation(theta[5], r[5], d[5], alpha[5]))
+
+    return Forward
+
+
+# Example usage:
+if __name__ == '__main__':   
+    
+    kinematic  = Forward_Kinematics([0,0,0,0,0,0]) 
+    print(kinematic)
+    
+    #Kinematic=Forward_Kinematics() 
+    #print(Kinematic)
+    
+    
