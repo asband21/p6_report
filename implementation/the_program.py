8da0af0b3163365c0c101de72d1011194c1e1253
jakob jørgensen <jakob.jorgensen01@gmail.com>
2024-04-19 08:44:25 +0200

    the program is updated and scout_path is made

--- implementation/the_program.py
+++ implementation/the_program.py
@@ -14,18 +14,22 @@ def info_extraction() :
 
     return joint_postion, x_tcp, y_tcp, z_tcp, Rx_tcp, Ry_tcp, Rz_tcp
     
-
 # tranform the  real world coordinates to the robot base frame usning tcp coordinates 
 def transformation(x_tcp, y_tcp, z_tcp,Rx_tcp,Ry_tcp,Rz_tcp,real_coordinates): 
     # The transformation matrix from the camera to the robot base frame with orientation 
     #T = np.array([[np.cos(Ry_tcp)*np.cos(Rz_tcp), np.cos(Rz_tcp)*np.sin(Rx_tcp)*np.sin(Ry_tcp)-np.cos(Rx_tcp)*np.sin(Rz_tcp), np.cos(Rx_tcp)*np.cos(Rz_tcp)*np.sin(Ry_tcp)+np.sin(Rx_tcp)*np.sin(Rz_tcp), x_tcp+0.035], [np.cos(Ry_tcp)*np.sin(Rz_tcp), np.cos(Rx_tcp)*np.cos(Rz_tcp)+np.sin(Rx_tcp)*np.sin(Ry_tcp)*np.sin(Rz_tcp), np.cos(Rx_tcp)*np.sin(Ry_tcp)*np.sin(Rz_tcp)-np.cos(Rz_tcp)*np.sin(Rx_tcp), y_tcp+0.105], [-np.sin(Ry_tcp), np.cos(Ry_tcp)*np.sin(Rx_tcp), np.cos(Rx_tcp)*np.cos(Ry_tcp), z_tcp-0.38], [0.0, 0.0, 0.0, 1.0]])
     
     #without orientation
-    T = np.array([[1, 0.0, 0.0, x_tcp], [0.0, 1, 0, y_tcp], [0.0, 0.0, 1, z_tcp], [0.0, 0.0, 0.0, 1.0]]) 
-    off_set = np.array([[1,0,0,0.035],[0,1,0,0.105],[0,0,1,-0.38],[0,0,0,1]]) 
+    T = np.array([[1, 0.0, 0.0, x_tcp], [0.0, 1, 0, y_tcp], [0.0, 0.0, 1, z_tcp], [0.0, 0.0, 0.0, 1.0]])  
+
+    # The offset between the camera and the robot base frame
+    off_set = np.array([[1,0,0,0.035],[0,1,0,0.105],[0,0,1,-0.38],[0,0,0,1]])  
+    # The transformation matrix from the camera to the robot base frame and offset
     T_off = T @ off_set 
 
-    real_coordinates_matrix = np.array([[1,0,0,-real_coordinates[0]],[0,1,0,-real_coordinates[1]],[0,0,1,real_coordinates[2]],[0,0,0,1]]) 
+    # The real_coordinates_matrix  x and y cooordiantes are flipped because of the camera frame 
+    real_coordinates_matrix = np.array([[1,0,0,-real_coordinates[0]],[0,1,0,-real_coordinates[1]],[0,0,1,real_coordinates[2]],[0,0,0,1]])  
+
     # The transformation matrix from the robot base frame to the camera frame
     T_inv = np.linalg.inv(T_off) 
     # The real world coordinates in the robot base frame
@@ -33,11 +37,8 @@ def transformation(x_tcp, y_tcp, z_tcp,Rx_tcp,Ry_tcp,Rz_tcp,real_coordinates):
     
     return real_coordinates_robot_base
 
-
 # From the camera, finds the real world coordiates of the center of the camera  
 joint_postion,x_tcp,y_tcp,z_tcp,Rx_tcp,Ry_tcp,Rz_tcp=info_extraction()
-real_coordinates = real_world_coordinates([360,620])
+real_coordinates = real_world_coordinates([360,620]) # incerte the disred pixel  in x,y coordinates
 real_coordinates_robot_base = transformation(x_tcp, y_tcp, z_tcp,Rx_tcp,Ry_tcp,Rz_tcp,real_coordinates) 
 print(real_coordinates_robot_base)  
-
-#[360, 640]
