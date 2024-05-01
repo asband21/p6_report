00ca370710c4f85be1b3a46acb23bc8336c2381d
Mads W Mikkelsen <mwmi19@student.aau.dk>
2024-05-01 11:07:51 +0200

    cleanup and added coord list with associated indices

--- PathSnapping/PathSnapping.py
+++ PathSnapping/PathSnapping.py
@@ -1,43 +1,22 @@
 import re
 
-
 with open(r'toolpath_Ideal.txt', 'r') as file:
     lines = file.readlines()
-
-#for line in lines:
- #   print(line.strip())
-
+array = [[], [], [], [], [], []]
 
 for index, line in enumerate(lines):
     # Check if the line contains the word "movel"
     if "movel" not in line:
         lines[index] = "false"
-        #print("false")
-    #else:
-        #print("true")
-
-
 for index, line in enumerate(lines):
 # Define a regular expression pattern to match the coordinates within square brackets
     pattern = r"\[(.*?), (.*?), (.*?), (.*?), (.*?), (.*?)\]"
-
 # Use the findall method of the re module to extract the coordinates
     matches = re.findall(pattern, lines[index])
-
     if matches:
-        x_coord = float(matches[0][0])
-        y_coord = float(matches[0][1])
-        z_coord = float(matches[0][2])
-        roll_coord = float(matches[0][3])
-        pitch_coord = float(matches[0][4])
-        yaw_coord = float(matches[0][5])
-        #print("Coordinates:", x_coord, y_coord, z_coord)
+        array[0].append(index)
+        for i in range(5):
+            array[i+1].append(float(matches[0][i]))
+for j in range(len(array[0])):
+    print("Coordinates:", array[1][j], array[2][j], array[3][j])
 
-
-
-array = [[],[],[],[],[],[]]
-print(array)
-#print(array[1][0])
-array[0].append(1)
-print(array)
-print(array[0][0])
