788e6a9e6d3f48836f7d2c66add6f7d269cf81f1
klyxa <kykutura@gmx.com>
2024-05-13 13:50:50 +0200

    the code compils

--- pcl/localization.cpp
+++ pcl/localization.cpp
@@ -230,9 +230,10 @@ Eigen::Matrix4f multi_ICP(const pcl::PointCloud<pcl::PointXYZ>::Ptr &source, con
 	return complete_transformation;
 }
 
-pcl::PointCloud<pcl::PointXYZ> c_arey_to_pcl_pc(int point_count, double *points);
+pcl::PointCloud<pcl::PointXYZ> c_arey_to_pcl_pc(int point_count, double *points)
 {
 
+	//auto cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
         pcl::PointCloud<pcl::PointXYZ> cloud;
         cloud.width    = point_count;
         cloud.height   = 1;
@@ -246,24 +247,29 @@ pcl::PointCloud<pcl::PointXYZ> c_arey_to_pcl_pc(int point_count, double *points)
 		point.z = *(points+i*3+2);
                 i++;
         }
-        icp_return cloud;
+        return cloud;
 }
 extern "C"
 {
         void lokailasiens(double* output,int scan_count, double *scan, int cad_count, double *cad)
         {
-	        pcl::PointCloud<pcl::PointXYZ>::Ptr cad_model(new pcl::PointCloud<pcl::PointXYZ>);
-	        pcl::PointCloud<pcl::PointXYZ>::Ptr scan(new pcl::PointCloud<pcl::PointXYZ>);
-                scan = cad_model(scan_count,scan);
-                cad_model = cad_model(cad_count,cad);
-	        Eigen::Matrix4f transformation = multi_ICP(scan, cad_model, 16);
-                transformation 
+		pcl::PointCloud<pcl::PointXYZ> cad_model_pcl;
+		pcl::PointCloud<pcl::PointXYZ> scan_pcl;
+                scan_pcl = c_arey_to_pcl_pc(scan_count, scan);
+                cad_model_pcl = c_arey_to_pcl_pc(cad_count, cad);
+		//skal roter lokalt
+		auto scan_pcl_sprt = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>(scan_pcl);
+        	auto cad_pcl_sprt = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>(cad_model_pcl);
+
+		VoxelGrid_homogenise(scan_pcl_sprt, 0.005f);
+		VoxelGrid_homogenise(cad_pcl_sprt, 0.005f);
+	        Eigen::Matrix4f transformation = multi_ICP(scan_pcl_sprt, cad_pcl_sprt, 16);
                 for (int i = 0; i < 4; i++)
                 {
-                        output[4*i + 0] = transformation[i][0];
-                        output[4*i + 1] = transformation[i][1];
-                        output[4*i + 2] = transformation[i][2];
-                        output[4*i + 3] = transformation[i][3];
+                        output[4*i + 0] = transformation(i ,0);
+                        output[4*i + 1] = transformation(i ,1);
+                        output[4*i + 2] = transformation(i ,2);
+                        output[4*i + 3] = transformation(i ,3);
                 }
         }
 }
