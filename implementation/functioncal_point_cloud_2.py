333af2d77f44ddd8eea1b83da83d3906106c23c1
jakob jørgensen <jakob.jorgensen01@gmail.com>
2024-05-17 14:18:00 +0200

    Code adjustment -> pointclod_2

--- implementation/functioncal_point_cloud_2.py
+++ implementation/functioncal_point_cloud_2.py
@@ -5,7 +5,7 @@ import pickle
 import time
 import zlib
 
-per_alo = True
+per_alo = True 
 
 def load_csv_to_numpy(filename):
     try:
@@ -85,10 +85,10 @@ def weld_item_to_globel_transformaisen(cam_fun = None):
     filename_cad = './gig_AAUTest97_ransed.csv'
     cad = load_csv_to_numpy(filename_cad)
 
-    num_iterations = 50
+    num_iterations = 5
     if per_alo:
         num_points_per_frame = 640 * 480  # 307200# Pre-allocate an array for all transformed vertices
-        all_transformed_vertices = np.empty((num_iterations * num_points_per_frame, 3), dtype=np.float32)
+        all_transformed_vertices = np.empty((num_iterations * num_points_per_frame, 3), dtype=np.float64)
     else:
         all_transformed_vertices = np.empty((0, 3))
 
@@ -118,28 +118,29 @@ def weld_item_to_globel_transformaisen(cam_fun = None):
             print(i)
             print(transformed_vertices.shape)
 
-            time.sleep(0.1)
+            #time.sleep(0.1)
 
         #print(all_transformed_vertices)
         #print(all_transformed_vertices.shape)
-        boks_min = np.array([0, 0, 0])
-        boks_max = np.array([1.1, 1.1, 1.1])
+        boks_min = np.array([-1000, -1000.0, -10000])
+        boks_max = np.array([1000000.1, 10000.1, 100000.1])
         scan = filter_points_inside_box(boks_min, boks_max, all_transformed_vertices)
         #print("scan")
-        #print(scan.shape)
+        print(scan.shape)
         #print("cad")
-        #print(cad)
+        print(cad.shape)
         #pipeline.stop()
         
         receiver_ip = '100.95.44.35'  # Replace with the actual IP address
         port = 65432  # Replace with the actual port
         result = send_arrays_and_receive_result(cad, scan, receiver_ip, port)
+        """
         if result is not None:
             print(result)
         else:
             print("Failed to receive the result")
         return result
-
+        """
     finally:
         pipeline.stop()
 
